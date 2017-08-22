677a6a0ae89eef57b57b285eab9e992f94c103c6
angelasubi <21978301@qq.com>
2017-08-22 17:48:32 +0800

    creat bookmodel

--- spaider.js
+++ spaider.js
@@ -2,3 +2,13 @@ var Crawler = require('crawler')
 
 var db = require('mongoose')
 db.connect('mongodb://localhost/books_db')
+
+var Book = db.model('book', {
+    title: String,
+    description: String,
+    img: String,
+    link: String,
+    price: String,
+    author: String,
+    publicsher: String
+})
