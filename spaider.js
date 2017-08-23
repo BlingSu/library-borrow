2c92e2f5f216a4944f80a108f2d5d7155022743b
angelasubi <21978301@qq.com>
2017-08-23 16:38:49 +0800

    完善crawler callback

--- spaider.js
+++ spaider.js
@@ -17,5 +17,19 @@ var spalider = new Crawler({
     maxConnections: 10,
     forceUTF8: true,
     incomingEncoding: 'gb2312',
-    callback: function (err, res, $) {}
+    callback: function (err, res, $) {
+        $('.bang_list li').each(function (index, item) {
+            var book = new Book()
+            book.title = $(item).find('.name a').text()
+            book.img = $(item).find('.pic a img').attr('src')
+            book.link = $(item).find('.pic a').attr('href')
+            book.price = Number(($(item).find('.price p span').eq(0).text()).replace('¥', ''))
+            book.author = $(item).find('.publisher_info a').eq(0).attr('title')
+            book.publisher = $(item).find('.publisher_info a').last().text()
+        })
+        book.save((err => {
+            if (err) { console.log(err) }
+                else { console.log('save is ok') }
+        })
+    }
 })
