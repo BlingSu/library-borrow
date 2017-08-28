8e3c808875844776d21d85d0cb2bfcad13b34ca3
angelasubi <21978301@qq.com>
2017-08-28 09:29:57 +0800

    引入art-tempalte use body-parser

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var path = require('path')
 
+var bodyParser = require('body-parser')
+var morgan = require('morgan')
+
 var app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('成功搭起页面')
