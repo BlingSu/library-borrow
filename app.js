dbfd44338488e77eb7c458d2e6ff954c2fbd810a
angelasubi <21978301@qq.com>
2017-08-28 11:34:10 +0800

    get list api

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var morgan = require('morgan')
+var path = require('path')
+var logger = require('morgan')
 
 var app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(morgan('dev'))
-
-app.use(express.static('./www'))
-
-_globalPath = __dirname
 
 /* art-template */
-const template = require('art-template')
+var template = require('art-template')
+template.config('base', '')
+template.config('extname', '.html')
+app.engine('.html', template.__express)
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', (req, res) => { res.redirect('api/list') })
+app.get('/', (req, res) => { res.redirect('/books/list') })
 
-app.use('/api',require('./controllers/index'))
+app.use('/books/',require('./routes/books'))
+
 
 app.listen(3333, () => {
     console.log('the server is running in 3333~~~')
