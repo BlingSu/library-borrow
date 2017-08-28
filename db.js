7b24acc8570e727b02a322b004b4e874cc81c3fb
angelasubi <21978301@qq.com>
2017-08-28 09:21:21 +0800

    连接数据创建模型

--- db.js
+++ db.js
@@ -1,1 +1,23 @@
 /* 数据 */
+
+var express = require('express')
+var db = require('mongoose')
+db.connect('mongodb://localhost/books_db')
+
+
+var Schema = db.Schema
+
+var blogSchema = new Schema({
+    title: String,
+    img: String,
+    link: String,
+    price: String,
+    author: String,
+    publicsher: String
+})
+
+var Blog = db.model('Blog', blogSchema)
+
+module.exports = {
+    Blog: Blog
+}
