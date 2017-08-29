1aa44f2d0fd0824f5b9adea3bac2a4e2770ce1b9
angelasubi <21978301@qq.com>
2017-08-29 13:56:49 +0800

    添加getdata接口

--- routes/books.js
+++ routes/books.js
@@ -13,6 +13,37 @@ router.get('/list', (req, res) => {
 
 router.get('/get_data/:page?', (req, res) => {
     console.log(req)
+    let currentPage = 1
+    let pageSize = 10
+    let keyWord = req.query.keyWord
+    let filter = {}
+
+    if (keyWord) {
+        filter = { title: new RegExp(keyWord, 'i') }
+    }
+    if (req.params.page) {
+        currentPage = Number(req.params.page)
+    }
+    if (currentPage <= 0) {
+        currentPage = 1
+    }
+    /*
+        limit 限制返回结果的数量
+        skip 跳过前几个，返回其余
+        sort 1正-1反
+     */
+    Book.find(filter).limit(pageSize).skip((currentPage - 1) * pageSize).sort({id:-1})
+        .then((resp) => {
+            console.log(data, '==========data')
+            if (resp.length > 0) {
+                res.json({ status: 'y', data: resp, current_page: currentPage })
+            } else {
+                res.json({ status: 'n', msg: '没有更多数据' })
+            }
+        })
+        .catch((err) => {
+            res.json({ status: 'n', data: [], msg: '获取数据失败' })
+        })
 })
 
 module.exports = router
