55ab9c54041b9fe03868a2aed743da955cbc8e89
angelasubi <21978301@qq.com>
2017-08-30 11:20:52 +0800

    创建student模型

--- db.js
+++ db.js
@@ -7,7 +7,6 @@ db.connect('mongodb://localhost/books_db', {useMongoClient : true})
 db.Promise = Promise
 
 var Schema = db.Schema
-
 var bookSchema = new Schema({
     title: String,
     img: String,
@@ -17,8 +16,32 @@ var bookSchema = new Schema({
     publicsher: String
 })
 
+var studentSchema = new Schema({
+    name: String,
+    gender: String,
+    birthday: {
+        type: Date,
+        default: Date.now()
+    },
+    user_name: String,
+    pwd: String,
+    mobile: String,
+    email: String,
+    address: String,
+    created_at: {
+        type: Date,
+        default: Date.now()
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
 var Book = db.model('Book', bookSchema)
+var Student = db.model('student', studentSchema)
 
 module.exports = {
-    Book: Book
+    Book: Book,
+    Student: Student
 }
